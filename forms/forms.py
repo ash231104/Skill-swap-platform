94d09c46f259cfd0a3c221c3bede5d9e088c05e5
Ashlesha Verma <vermaashleshaverma@gmail.com>
2025-07-12 15:03:52 +0530

    Fix circular imports and update model, forms, extensions

--- forms/forms.py
+++ forms/forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, TextAreaField, SelectField
 from wtforms.validators import DataRequired, Email, EqualTo, Length
 
 class RegistrationForm(FlaskForm):
@@ -14,3 +14,24 @@ class LoginForm(FlaskForm):
     password = PasswordField('Password', validators=[DataRequired()])
     remember = BooleanField('Remember Me')
     submit = SubmitField('Login')
+
+class ProfileForm(FlaskForm):
+    location = StringField('Location')
+    skills_offered = TextAreaField('Skills Offered', validators=[DataRequired()])
+    skills_wanted = TextAreaField('Skills Wanted', validators=[DataRequired()])
+    availability = StringField('Availability')
+    is_private = BooleanField('Private Profile')
+    submit = SubmitField('Save Changes')
+
+class SwapRequestForm(FlaskForm):
+    skill = StringField('Skill', validators=[DataRequired()])
+    submit = SubmitField('Send Request')
+
+class FeedbackForm(FlaskForm):
+    rating = SelectField('Rating', choices=[(1,'1'),(2,'2'),(3,'3'),(4,'4'),(5,'5')], coerce=int)
+    comment = TextAreaField('Comment')
+    submit = SubmitField('Submit Feedback')
+
+class AnnouncementForm(FlaskForm):
+    message = TextAreaField('Announcement Message', validators=[DataRequired()])
+    submit = SubmitField('Send Announcement')
--- models/models.py
+++ models/models.py
@@ -1,4 +1,4 @@
-from app import db
+from extensions import db
 from flask_login import UserMixin
 
 class User(db.Model, UserMixin):
@@ -30,42 +30,3 @@ class AdminLog(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     action = db.Column(db.String(200))
     timestamp = db.Column(db.DateTime, default=db.func.current_timestamp())
-
-#--- forms/forms.py --- # type: ignore
-from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField, TextAreaField, SelectField
-from wtforms.validators import DataRequired, Email, EqualTo, Length
-
-class RegistrationForm(FlaskForm):
-    name = StringField('Name', validators=[DataRequired()])
-    email = StringField('Email', validators=[DataRequired(), Email()])
-    password = PasswordField('Password', validators=[DataRequired(), Length(min=6)])
-    confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
-    submit = SubmitField('Register')
-
-class LoginForm(FlaskForm):
-    email = StringField('Email', validators=[DataRequired(), Email()])
-    password = PasswordField('Password', validators=[DataRequired()])
-    remember = BooleanField('Remember Me')
-    submit = SubmitField('Login')
-
-class ProfileForm(FlaskForm):
-    location = StringField('Location')
-    skills_offered = TextAreaField('Skills Offered', validators=[DataRequired()])
-    skills_wanted = TextAreaField('Skills Wanted', validators=[DataRequired()])
-    availability = StringField('Availability')
-    is_private = BooleanField('Private Profile')
-    submit = SubmitField('Save Changes')
-
-class SwapRequestForm(FlaskForm):
-    skill = StringField('Skill', validators=[DataRequired()])
-    submit = SubmitField('Send Request')
-
-class FeedbackForm(FlaskForm):
-    rating = SelectField('Rating', choices=[(1,'1'),(2,'2'),(3,'3'),(4,'4'),(5,'5')], coerce=int)
-    comment = TextAreaField('Comment')
-    submit = SubmitField('Submit Feedback')
-
-class AnnouncementForm(FlaskForm):
-    message = TextAreaField('Announcement Message', validators=[DataRequired()])
-    submit = SubmitField('Send Announcement')
